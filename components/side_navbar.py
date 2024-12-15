e9c8005352234b7fd244813dce4fba8dfa2678d9
Ibrahim Haizel <90156827+Ibrahim-Haizel@users.noreply.github.com>
2024-12-15 19:17:53 +0000

    Add functions for sidebar navigation links with active state support

--- components/side_navbar.py
+++ components/side_navbar.py
@@ -19,3 +19,30 @@ def side_navbar(
         id=nav_id,
         style = {"width":"100%", 'margin-right': '60px'}
     )
+
+def side_navbar_link(text, href, style = None):
+    """A link to another dashboard"""
+    return html.Li(
+        dcc.Link(text, href=href, className="govuk-link govuk-link--no-visited-state", style = style),
+        className="moj-side-navigation__item govuk-!-margin-bottom-1",
+    )
+
+
+def side_navbar_link_active(text, href, style = None):
+    """
+    A link to another dashboard that appears highlighted, suggesting to the user that they are
+    already viewing the linked dashboard.
+    """
+    return html.Li(
+        html.A(
+            text,
+            href=href,
+            className="govuk-link govuk-link--no-visited-state",
+            **{"aria-current": "true"},
+            style = style
+        ),
+        className=(
+            "moj-side-navigation__item moj-side-navigation__item--active "
+            "govuk-!-margin-bottom-1"
+        ),
+    )
