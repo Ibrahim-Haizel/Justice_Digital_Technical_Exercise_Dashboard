ef29bedd6dc1c6e01beb74d79c009c64913fefa1
Ibrahim Haizel <90156827+Ibrahim-Haizel@users.noreply.github.com>
2024-12-22 23:18:04 +0000

    Add 404 Not Found page layout
    - Implemented a new `not_found_404.py` page with a user-friendly layout.
    - Included guidance for users on navigating back to the homepage and contacting support.
    - Ensured compliance with GDS styling for consistency across the application.

---
+++ pages/not_found_404.py
@@ -0,0 +1,36 @@
+import dash
+from dash import html
+from constants import EMAIL_ADDRESS
+
+dash.register_page(__name__)
+
+layout = html.Div([
+    html.Br(),
+    html.Br(),
+    html.Main(className="govuk-main-wrapper govuk-main-wrapper--l", role="main", children=[
+        html.Div(className="govuk-grid-row", children=[
+            html.Div(className="govuk-grid-column-two-thirds", children=[
+                html.H1("Page not found", className="govuk-heading-l"),
+                html.Br(),
+                html.P("If you typed the web address, check it is correct.", className="govuk-body"),
+                html.P("If you pasted the web address, check you copied the entire address.", className="govuk-body"),
+                html.P(
+                    [
+                        "If the web address is correct or you selected a link or button, try refreshing the page, or going back to ",
+                        html.A("the dashboard homepage", href="/home", className="govuk-link"),
+                        "."
+                    ],
+                    className="govuk-body",
+                ),
+                html.P(
+                    [
+                        "Contact the team on ",
+                        html.A(EMAIL_ADDRESS, href=f"mailto:{EMAIL_ADDRESS}", className="govuk-link"),
+                        " if this keeps happening."
+                    ],
+                    className="govuk-body",
+                ),
+            ])
+        ])
+    ])
+]) 
